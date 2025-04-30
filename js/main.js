9dd66a5f146fb92a29c906f7d00b0d94299cb236
Miller Zhang <yufeng.official@gmail.com>
2025-04-30 22:21:23 +0800

    the best practice

--- js/main.js
+++ js/main.js
@@ -1,47 +1,42 @@
-let rand = function () {
-    if (!window.crypto || !window.crypto.getRandomValues)
-        return Math.floor(Math.random() * 100000);
-    else return window.crypto.getRandomValues(new Uint32Array(1))[0] % 100000;
+'use strict';
+
+let rand = () => window.crypto.getRandomValues(new Uint32Array(1))[0] % 100000;
+
+if (!window.crypto || !window.crypto.getRandomValues) {
+    console.warn('Crypto API not supported, using Math.random() instead.');
+    rand = () => Math.floor(Math.random() * 100000);
 }
 
-let randLetter = function () {
-    return String.fromCharCode('a'.charCodeAt(0) + rand() % 26);
-}
+const randLetter = () => String.fromCharCode('a'.charCodeAt(0) + rand() % 26);
 
-let randNumber = function () {
-    return String.fromCharCode('0'.charCodeAt(0) + rand() % 10);
-}
+const randNumber = () => String.fromCharCode('0'.charCodeAt(0) + rand() % 10);
 
-let randPwd = function () {
+const randPwd = () => {
     let pwd = '-****-****-****'.split('');
     pwd[rand() % 11 > 3 ? rand() % 2 * 5 + 6 + rand() % 4 : rand() % 3 + 2] = randNumber();
     let cnt = 0;
-    for (let i = 0; i < pwd.length; i++) {
-        if (pwd[i] == '-') {
+    pwd.forEach((char, index) => {
+        if (char == '-')
             cnt = 0;
-        } else {
-            if (pwd[i] == '*') {
+        else {
+            cnt++;
+            if (char == '*') {
                 let c = '';
                 let flag = true;
                 while (flag) {
                     flag = false;
                     c = randLetter();
-                    for (let j = 1; j < cnt; j++) {
-                        if (pwd[i - j] == c) {
-                            flag = true;
-                            break;
-                        }
-                    }
+                    let j = 0;
+                    while (++j < cnt && !(flag = pwd[index - j] == c));
                 }
-                pwd[i] = c;
+                pwd[index] = c;
             }
-            cnt++;
         }
-    }
+    });
     return pwd.join('').slice(1);
 }
 
-window.onload = function () {
-    let pwd = randPwd();
-    document.getElementById("rand-pwd").textContent = pwd;
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const genedPwd = randPwd();
+    document.getElementById("rand-pwd").textContent = genedPwd;
+});
